68f2bc0c030a0e59e14a4bdf1bac0f84c78505e4
philip <philip.clarke@redhood.co.uk>
2015-12-15 15:44:04 +0000

    started file read

---
+++ program3.js
@@ -0,0 +1,8 @@
+var fs = require('fs');
+var count = undefined;
+
+fs.readdir(process.argv[2], function(err, list){
+var files = files.split('md').length - 1
+console.log(files);
+console.log(process.argv[3]);
+})
